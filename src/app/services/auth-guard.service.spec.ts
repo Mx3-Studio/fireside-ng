d581dbd9a33d2b3ffab86a3d0705bfd31c2a11bf
Michael Dowden <michael@dowden.us>
2017-10-12 01:15:27 -0400

    Fixed route check on AuthGuard

--- src/app/services/auth-guard.service.spec.ts
+++ src/app/services/auth-guard.service.spec.ts
@@ -1,28 +1,17 @@
-import { TestBed, inject, async, fakeAsync, tick } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Mock } from 'ts-mocks';
 
 import * as firebase from 'firebase/app';
 import { Observable } from 'rxjs';
-import { Component } from '@angular/core';
-import { Location } from "@angular/common";
-import { Router, Routes } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthenticationService } from './authentication.service';
 
 import { AuthGuardService } from './auth-guard.service';
 
-@Component({ template: `<router-outlet></router-outlet>` })
-class AppComponent { }
-
-@Component({ template: `<h1>Home</h1>` })
-class HomeComponent { }
-
-@Component({ template: `<h1>Login</h1>` })
-class LoginComponent { }
-
 describe('AuthGuardService', () => {
 
-  describe('works with a valid user', () => {
+  describe('canActivate a valid user', () => {
     let mockAuthService: Mock<AuthenticationService>;
     let authService: AuthenticationService;
     const user: firebase.UserInfo = {
@@ -50,94 +39,39 @@ describe('AuthGuardService', () => {
       }).compileComponents();
     });
 
-    it('should be created', inject([AuthGuardService], (service: AuthGuardService) => {
-      expect(service).toBeTruthy();
-    }));
-
-    it('should return canActivate true for valid user', inject([AuthGuardService], (service: AuthGuardService) => {
+    it('returns true', inject([AuthGuardService], (service: AuthGuardService) => {
       service.canActivate().subscribe(result => expect(result).toBe(true));
     }));
 
   });
 
-  describe('behaves as expected with no authentication', () => {
-  
-    const routes: Routes = [
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
-      { path: 'login', component: LoginComponent }
-    ];
-
+  describe('canActivate with no authentication', () => {
     let mockAuthService: Mock<AuthenticationService>;
     let authService: AuthenticationService;
 
-    let location: Location;
-    let router: Router;
-    let fixture;
-
-    beforeEach( async(() => {
+    beforeEach(() => {
       mockAuthService = new Mock<AuthenticationService>();
       mockAuthService.setup(o => o.userInfo).is(Observable.of(null));
       authService = mockAuthService.Object;
 
       TestBed.configureTestingModule({
-        declarations: [ AppComponent, HomeComponent, LoginComponent ],
-        imports: [
-          RouterTestingModule.withRoutes(routes)
-        ],
+        imports: [ RouterTestingModule ],
         providers: [
           { provide: AuthenticationService, useValue: authService },
           AuthGuardService
         ]
-      }); //.compileComponents();
-
-      router = TestBed.get(Router);
-      location = TestBed.get(Location);
-      fixture = TestBed.createComponent(AppComponent);
-      router.initialNavigation();
-    }));
-
-    it('should return canActivate false', inject([AuthGuardService], (service: AuthGuardService) => {
-      service.canActivate().subscribe(result => expect(result).toBe(false));
-    }));
-
-    // This doesn't correctly pick up the route change for some reason
-    // See: https://codecraft.tv/courses/angular/unit-testing/routing/
-    // More testing examples here - https://embed.plnkr.co/?show=preview
-    it('should navigate to /login', fakeAsync(inject([AuthGuardService], (service: AuthGuardService) => {
-      //router.navigate(['login']);
-      service.canActivate(); //.subscribe(result => {
-      //   expect(result).toBe(false)
-      //   expect(location.path()).toBe('/login');
-      // })
-      // Perhaps should use the following syntax?
-      tick(50);
-      expect(location.path()).toBe('/login');
-    })));
-
-    // This also doesn't work and is exactly copied from the above demo
-    it('navigate to "" redirects you to /home', fakeAsync(() => {
-      router.navigate(['']);
-      tick(50);
-      expect(location.path()).toBe('/home');
-    }));
-
-    // This also doesn't work and is exactly copied from the above demo
-    it('navigate to "search" takes you to /search', fakeAsync(() => {
-      router.navigate(['/home']);
-      tick(50);
-      expect(location.path()).toBe('/home');
-    }));
-
-    // This works from the example
-    it('fakeAsync works', fakeAsync(() => {
-      let promise = new Promise((resolve) => {
-        setTimeout(resolve, 10)
       });
-      let done = false;
-      promise.then(() => done = true);
-      tick(50);
-      expect(done).toBeTruthy();
+    });
+
+    // Found this solution here - https://stackoverflow.com/questions/41496194/unit-testing-angular-2-authguard-spy-method-is-not-being-called
+    it('returns false and navigates to /login', inject([AuthGuardService, Router], (service: AuthGuardService, router: Router) => {
+      // add a route spy
+      spyOn(router, 'navigate');
+
+      service.canActivate().subscribe(result => {
+        expect(result).toBe(false);
+        expect(router.navigate).toHaveBeenCalledWith([ '/login' ]);
+      });
     }));
 
   });
