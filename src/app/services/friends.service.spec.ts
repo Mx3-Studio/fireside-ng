bc82f1e9bd0d399caa7c24a827ff2847a29ceb1f
Michael Dowden <michael@dowden.us>
2017-11-24 03:10:14 -0500

    Added friends test

--- src/app/services/friends.service.spec.ts
+++ src/app/services/friends.service.spec.ts
@@ -1,20 +1,26 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { Mock } from 'ts-mocks';
 
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { ProfileService } from './profile.service';
 
 import { FriendsService } from './friends.service';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 describe('FriendsService', () => {
   let mockAngularFireDb: Mock<AngularFireDatabase>;
   let mockProfileSvc: Mock<ProfileService>;
-  
-    beforeEach(() => {
-      mockAngularFireDb = new Mock<AngularFireDatabase>();
-      mockProfileSvc = new Mock<ProfileService>();
+  let results: Observable<any[]> = new BehaviorSubject([{
 
-      TestBed.configureTestingModule({
+  }]);
+
+  beforeEach(() => {
+    mockAngularFireDb = new Mock<AngularFireDatabase>();
+    mockAngularFireDb.setup(o => o.list).is((path, opts) => new FirebaseListObservable<any[]>(null, results.subscribe));
+    mockProfileSvc = new Mock<ProfileService>();
+
+    TestBed.configureTestingModule({
       providers: [
         FriendsService,
         { provide: AngularFireDatabase, useValue: mockAngularFireDb.Object },
@@ -28,7 +34,8 @@ describe('FriendsService', () => {
   }));
 
   it('should return a list of friends', inject([FriendsService], (service: FriendsService) => {
-
+    let list = service.search("abc@gmail.com");
+    expect(list.toArray).toBe(results.toArray);
   }));
 
 });
